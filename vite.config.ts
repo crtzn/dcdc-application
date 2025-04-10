340cc9b412157cef3cb32c033f443ec8ff410d79
Cortezano <cortezano.dvcode@gmail.com>
2025-04-10 20:14:42 +0800

    📦

--- vite.config.ts
+++ vite.config.ts
@@ -1,14 +1,22 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import tailwindcss from "@tailwindcss/vite";
+import path from "path";
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [tailwindcss(), react()],
+  plugins: [react(), tailwindcss()],
   base: "./",
   build: {
     outDir: "dist-react",
   },
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "./src"),
+    },
+    extensions: [".js", ".jsx", ".ts", ".tsx"],
+  },
+
   server: {
     port: 5123,
     strictPort: true,
